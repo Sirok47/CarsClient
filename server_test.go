ade737712b93cd8af0f9a9c338c9204a85bc2549
maksim <xromosoma.47-maya@yandex.by>
2021-05-19 16:21:40 +0300

    .....

--- server_test.go
+++ server_test.go
@@ -3,14 +3,18 @@ package main
 import (
 	"CarsClient/handler"
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	protocol "github.com/Sirok47/CarsServer/protocol"
+	"github.com/Sirok47/CarsServer/service"
 	"github.com/dgrijalva/jwt-go"
+	"github.com/jackc/pgx/v4"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"google.golang.org/grpc"
 	"io"
+	"net"
 	"net/http"
 	"os"
 	"testing"
@@ -18,14 +22,27 @@ import (
 )
 
 var (
-	body []byte
-	//err error
-	token string
-	cli   *http.Client
+	dbconn *pgx.Conn
+	body   []byte
+	err    error
+	token  string
+	cli    *http.Client
 )
 
-//req.Header.Set("Authorization",fmt.Sprintf("%s %s","Bearer",token))
 func TestMain(m *testing.M) {
+	dbconn, err = pgx.Connect(context.Background(), "postgres://maks:glazirovanniisirok@127.0.0.1:5432/cars")
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+	srv := grpc.NewServer()
+	srvobj := service.NewService(dbconn)
+	protocol.RegisterCarsServer(srv, srvobj)
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Print(err)
+	}
+	go func() { err = srv.Serve(l) }()
 	TokenValidation := middleware.JWTWithConfig(middleware.JWTConfig{
 		SigningKey: []byte("sirok"),
 	})
@@ -58,7 +75,35 @@ func TestMain(m *testing.M) {
 	os.Exit(exitCode)
 }
 
+func TestSignUp(t *testing.T) {
+	defer dbconn.Exec(context.Background(), "delete from users")
+	reqbody, _ := json.Marshal(
+		map[string]string{
+			"Nick":     "keklik",
+			"Password": "qpwoeirutyM123",
+		})
+	req, err := http.NewRequest("POST", "http://localhost:1323/user/signup", bytes.NewBuffer(reqbody))
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := cli.Do(req)
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err = io.ReadAll(resp.Body)
+	strbody := string(body)
+	if strbody != "New user added" {
+		t.Errorf("Got error: %v", strbody)
+	}
+}
+
 func TestLogIn(t *testing.T) {
+	dbconn.Exec(context.Background(), "insert into users (nick, password) values ($1,$2)", "keklik", "qpwoeirutyM123")
+	defer dbconn.Exec(context.Background(), "delete from users")
 	reqbody, _ := json.Marshal(
 		map[string]string{
 			"Nick":     "keklik",
@@ -79,15 +124,116 @@ func TestLogIn(t *testing.T) {
 	body, err = io.ReadAll(resp.Body)
 	strbody := string(body)
 	if strbody == "rpc error: code = Unknown desc = code=401, message=Unauthorized" {
-		t.Errorf("Expected token, got %v", string(body))
+		t.Errorf("Expected token, got %v", strbody)
 	}
 }
 
-//for{
-//bs := make([]byte, 1014)
-//n, err := resp.Body.Read(bs)
-//strbody=fmt.Sprintf("%s%s",strbody,string(bs[:n]))
-//if n == 0 || err != nil{
-//break
-//}
-//}
+func TestCreate(t *testing.T) {
+	defer dbconn.Exec(context.Background(), "delete from cars")
+	reqbody, _ := json.Marshal(
+		map[string]interface{}{
+			"CarBrand":  "test",
+			"CarNumber": 1234,
+			"Type":      "test",
+			"Mileage":   1000,
+		})
+	req, err := http.NewRequest("POST", "http://localhost:1323/car/create", bytes.NewBuffer(reqbody))
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+		return
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("%s %s", "Bearer", token))
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := cli.Do(req)
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err = io.ReadAll(resp.Body)
+	strbody := string(body)
+	if strbody != "Car have been created" {
+		t.Errorf("Got error: %v", strbody)
+	}
+}
+
+func TestGet(t *testing.T) {
+	dbconn.Exec(context.Background(), "insert into cars (carbrand,carnumber,type,mileage) values ($1,$2,$3,$4)", "test", 1234, "test", 1000)
+	defer dbconn.Exec(context.Background(), "delete from cars")
+	reqbody, _ := json.Marshal(
+		map[string]interface{}{
+			"CarNumber": 1234,
+		})
+	req, err := http.NewRequest("GET", "http://localhost:1323/car/get", bytes.NewBuffer(reqbody))
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+		return
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("%s %s", "Bearer", token))
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := cli.Do(req)
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err = io.ReadAll(resp.Body)
+	strbody := string(body)
+	if strbody != "{\"CarBrand\":\"test\",\"CarNumber\":1234,\"Mileage\":1000,\"CarType\":\"test\"}\n" {
+		t.Errorf("Expected {\"CarBrand\":\"test\",\"CarNumber\":1234,\"Mileage\":1000,\"CarType\":\"test\"}\n got: %v.", strbody)
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	dbconn.Exec(context.Background(), "insert into cars (carbrand,carnumber,type,mileage) values ($1,$2,$3,$4)", "test", 1234, "test", 1111)
+	defer dbconn.Exec(context.Background(), "delete from cars")
+	reqbody, _ := json.Marshal(
+		map[string]interface{}{
+			"CarNumber": 1234,
+			"mileage":   1112,
+		})
+	req, err := http.NewRequest("PUT", "http://localhost:1323/car/update", bytes.NewBuffer(reqbody))
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+		return
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("%s %s", "Bearer", token))
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := cli.Do(req)
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err = io.ReadAll(resp.Body)
+	strbody := string(body)
+	if strbody != "Car updated" {
+		t.Errorf("Got error %v", strbody)
+	}
+}
+func TestDelete(t *testing.T) {
+	dbconn.Exec(context.Background(), "insert into cars (carbrand,carnumber,type,mileage) values ($1,$2,$3,$4)", "brand", 1234, "type", 1111)
+	defer dbconn.Exec(context.Background(), "delete from cars")
+	reqbody, _ := json.Marshal(
+		map[string]interface{}{
+			"CarNumber": 1234,
+		})
+	req, err := http.NewRequest("DELETE", "http://localhost:1323/car/delete", bytes.NewBuffer(reqbody))
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+		return
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("%s %s", "Bearer", token))
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := cli.Do(req)
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err = io.ReadAll(resp.Body)
+	strbody := string(body)
+	if strbody != "Car deleted" {
+		t.Errorf("Got error %v", strbody)
+	}
+}
